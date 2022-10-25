ccf2ab123b2d475233318d4285c22dab967811c4
omaromarsalhi <omar.salhi@esprit.tn>
2022-10-25 20:31:08 +0100

    code

--- controller/users_DBC.php
+++ controller/users_DBC.php
@@ -1,16 +1,20 @@
 <?php
 if(isset($_POST['subb'])){
     require 'connection.php';
-    $firstName=$_POST['firstName'];
-    $lastName=$_POST['lastName'];
-    $email=$_POST['email'];
-    $dateOfBirth=$_POST['dateOfBirth'];
-    $password=$_POST['password'];
-    $phNumber=$_POST['phNumber'];
-    echo "finally";
     $query = $pdo ->prepare(
-        'INSERT INTO users_db(firstName,lastName,email,dateOfBirth,password,phNumber)
-        VALUES ($firstName,$lastName,$email,$dateOfBirth,$password,$phNumber)'
+        'INSERT INTO users_db(firstName,lastName,email,dateOfBirth,pasword,phNumber)
+        VALUES (:firstName,:lastName,:email,:dateOfBirth,:pasword,:phNumber)'
     );
+    $query->execute([
+        'firstName'=>$_POST['firstName'],
+        'lastName'=>$_POST['lastName'],
+        'email'=>$_POST['email'],
+        'dateOfBirth'=>$_POST['dateOfBirth'],
+        'pasword'=>$_POST['password'],
+        'phNumber'=>$_POST['phNumber']
+    ]);
+    $select = "SELECT * FROM users_db";
+    $result = $conn->query($select);
+    echo $select;
 }
 ?>
--- model/users_DB.php
+++ model/users_DB.php
@@ -7,5 +7,16 @@ class Users_DB{
     public $password;
     public $phNumber;
 
+
+    public function __construct($firstName,$lastName,$email,$dateOfBirth,$password,$phNumber)
+    {
+        $this->firstName=$firstName;
+        $this->lastName=$lastName;
+        $this->email=$email;
+        $this->dateOfBirth=$dateOfBirth;
+        $this->password=$password;
+        $this->phNumber=$phNumber;
+    }
+
 }
 ?>
